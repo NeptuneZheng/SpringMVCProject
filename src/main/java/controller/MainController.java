62af0b7975a5a04be3d5e67f50c80940a6945f42
NeptuneZheng <neptune.zheng@cargosmart.com>
2017-09-17 22:21:54 +0800

    init spring mvc project

---
+++ src/main/java/controller/MainController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Z.Neptune on 2017/9/17.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
